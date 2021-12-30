ecdf25eab6febf46627a0712ad27b3826ed18598
Riyaskansa <96871709+Riyaskansa@users.noreply.github.com>
2021-12-30 17:57:05 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+package week1.day2;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int num=13;
+        if(num%2==0) {
+        	System.out.println(num+"is even");
+        }
+        else {
+        	System.out.println(num+"is Odd");
+        }
+	}
+
+}
